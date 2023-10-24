f7f828a71db46d4503cce973a3e0722dab1203ac
CharlesWGillespie <charlesgillespiew@gmail.com>
2023-10-24 13:13:05 -0400

    added function to console.log data given from api call

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-const url = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={f64d56583e38e61579f73e892561d4ae}`
+let APIKey = "40ac34cf8262f576b914adb248d863e2";
+let userInput = document.querySelector("#userInput");
+let searchBtn = document.querySelector("#searchBtn");
+let city = userInput.value;
+
+let queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
+
+function search(event) {
+  event.preventDefault();
+  fetch(queryURL).then(function (response) {
+    return response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+}
+searchBtn.addEventListener("click", search);
