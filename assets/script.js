5ccfad3ce46192be51f9caf1599ee061073b4d98
CharlesWGillespie <charlesgillespiew@gmail.com>
2023-10-28 16:49:51 -0400

    function to display city name, temp, wind speed, and humidity

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,48 @@
-let APIKey = "40ac34cf8262f576b914adb248d863e2";
-let userInput = document.querySelector("#userInput");
-let searchBtn = document.querySelector("#searchBtn");
+// Define your API key and API endpoint here
+const APIKey = "40ac34cf8262f576b914adb248d863e2";
+const searchBtn = document.querySelector("#searchBtn");
 
-function search(event) {
-  event.preventDefault();
-
-  // Get the user's input (city name) from the input field
-  let city = userInput.value;
-
-  // Create the API request URL with the user's input
-  let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
-
-  // Perform the API request
+function fetchWeatherData(city) {
+  const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`; // Celsius
   fetch(queryURL)
-    .then(function (response) {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        // Handle error responses (e.g., city not found)
-        console.error("Error: City not found or other API issue");
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("City not found");
       }
+      return response.json();
     })
-    .then(function (data) {
-      // Process and display the weather data
-      console.log(data);
+    .then((data) => {
+      showWeather(data);
     })
-    .catch(function (error) {
-      console.error("An error occurred while fetching data:", error);
+    .catch((error) => {
+      alert(error.message);
     });
 }
 
-searchBtn.addEventListener("click", search);
+function showWeather(data) {
+  const { name } = data;
+  const { temp } = data.main;
+  const { speed } = data.wind;
+  const { humidity } = data.main;
 
+  // Convert temperature to Fahrenheit
+  const tempFahrenheit = (temp * 9) / 5 + 32;
+
+  const card = document.querySelector(".card");
+  const cardHeader = card.querySelector(".card-header");
+  const tempElement = card.querySelector(".list-group-item:nth-child(1)");
+  const windElement = card.querySelector(".list-group-item:nth-child(2)");
+  const humidityElement = card.querySelector(".list-group-item:nth-child(3)");
+
+  cardHeader.textContent = `City: ${name}`;
+  tempElement.textContent = `Temp (C): ${temp}°C | Temp (F): ${tempFahrenheit.toFixed()}°F`;
+  windElement.textContent = `Wind: ${speed} m/s`;
+  humidityElement.textContent = `Humidity: ${humidity}%`;
+}
+
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // Prevent form submission
+  const userInput = document.getElementById("userInput");
+  const city = userInput.value;
+  fetchWeatherData(city);
+});
